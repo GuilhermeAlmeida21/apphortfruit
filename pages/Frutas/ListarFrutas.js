cd2be62012abd7cb7b4bf9b23cfb8231c2672b7f
GUILHERME SOARES DE ALMEIDA <guisoares.almeida8@gmail.com>
2022-07-13 11:04:57 -0300

    first commit

---
+++ pages/Frutas/ListarFrutas.js
@@ -0,0 +1,81 @@
+import React, { useEffect, useState } from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { Text, View, StyleSheet, FlatList, TouchableOpacity, Alert } from 'react-native';
+import { buscarTodasFrutas } from './ModelFrutas';
+
+
+export default function ListarFrutas({navigation}) {
+
+  const [dadosFrutas, setdadosFrutas] = useState([]);
+
+  useEffect(async () => {
+    const resp = await buscarTodasFrutas()
+    setdadosFrutas(resp);
+  }, []);
+
+  return (
+    <View style={estilo.container}>
+
+      <Text style={estilo.titulo}>Lista de Frutas</Text>
+
+      <TouchableOpacity style={estilo.botaoCadFruta} onPress={()=> navigation.navigate('Cadastrar')} >
+        <Text style={estilo.botaoTextoCadFruta}>Cadastrar Fruta</Text>
+      </TouchableOpacity>
+      <StatusBar style="auto" />
+      
+      <FlatList
+        data={dadosFrutas}
+        keyExtractor={dadosFrutas => dadosFrutas.id}
+        renderItem={({ item }) =>
+          <TouchableOpacity onPress={()=> navigation.navigate('Alterar')}>
+            <View style={estilo.grupoFrutas}>
+
+              <Text style={estilo.textoBotaoFruta}>{item.fruta}</Text>
+              <Text style={estilo.textoBotaoValor}>{item.valor}</Text>
+
+            </View>
+          </TouchableOpacity>
+
+        }
+      />
+    </View>
+  );
+}
+
+
+const estilo = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#e9c46a',
+  },
+  titulo: {
+    fontSize: 30,
+    color: "#2a9d8f",
+    textAlign: 'center',
+    justifyContent: 'center',
+    marginVertical: 20
+  },
+  grupoFrutas: {
+    backgroundColor: '#2a9d8f',
+    margin: 15,
+    padding: 5,
+    borderRadius: 10,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  textoBotaoFruta: {
+    fontSize: 20
+  },
+  textoBotaoValor: {
+    fontSize: 20
+  },
+  botaoCadFruta:{
+    backgroundColor: '#2a9d8f',
+    margin: 15,
+    padding: 5,
+    borderRadius: 10,
+  },
+  botaoTextoCadFruta:{
+    fontSize: 18
+  }
+});
